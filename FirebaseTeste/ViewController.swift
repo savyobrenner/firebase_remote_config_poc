9ff945bfbb7e8f42bc8e8f7b16525e7f1a1d605d
Savyo Brenner <savyobrenner@gmail.com>
2021-08-25 14:56:25 -0300

    Initial Commit

---
+++ FirebaseTeste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseTeste
+//
+//  Created by Brenner on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
